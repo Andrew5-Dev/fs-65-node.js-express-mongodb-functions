8ae7d7b3892d7c69eddd32da94452b0d7ea5e0b7
Andrew5-Dev <topchiy.ap@gmail.com>
2024-09-20 11:01:18 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,23 @@
+#  API on Node.js і Express.js. use `celebrate` and `Joi` for validation.
+
+## Instructions
+
+npm install
+
+Run Development Server: 
+
+- npm run dev. 
+
+Run Production Server:
+
+
+- npm run start
+
+- The server is listening on port 3000
+
+Main Server routes:
+
+'/users' Mongo DB Integration GET, POST, DELETE, PUT requests
+
+For test requests: POSTMAN https://www.postman.com
+
---
+++ src/controllers/userControllers.mjs
@@ -0,0 +1,126 @@
+import { ObjectId } from 'mongodb'
+import { connectDB } from '../db.mjs'
+
+export const createUser = async (req, res, next) => {
+    try {
+        const db = await connectDB()
+        const users = db.collection('users')
+        const result = await users.insertOne(req.body)
+        res.status(201).send(`User created with id ${result.insertedId}`)
+    } catch (error) {
+        next(error)
+    }
+}
+
+export const createUsers = async (req, res, next) => {
+    try {
+        const db = await connectDB()
+        const users = db.collection('users')
+        const result = await users.insertMany(req.body)
+        res.status(201).send(`Users created with ids ${result.insertedIds}`)
+    } catch (error) {
+        next(error)
+    }
+}
+export const getUser = async (req, res, next) => {
+    try {
+        const db = await connectDB()
+        const users = db.collection('users')
+        const result = await users.find({}).toArray()
+        res.status(200).send(result)
+    } catch (error) {
+        next(error)
+    }
+}
+
+export const getUserById = async (req, res, next) => {
+    try {
+        const db = await connectDB()
+        const users = db.collection('users')
+        const result = await users.findOne({ _id: new ObjectId(req.params.id) })
+        if (!result) {
+            return res.status(404).send('User not found')
+        }
+        res.status(200).send(result)
+    } catch (error) {
+        next(error)
+    }
+}
+export const getUsers = async (req, res, next) => {
+    try {
+        const db = await connectDB()
+        const users = db.collection('users')
+        const result = await users
+          .find({ age: { $gte: 30 } })
+          .project({ name: 1, age: 1 })
+          .toArray()
+        res.status(200).send(result)
+    } catch (error) {
+        next(error)
+    }
+}
+
+export const getUsersByAge = async (req, res, next) => {
+    try {
+        const db = await connectDB()
+        const users = db.collection('users')
+        const result = await users
+            .find({ age: { $gte: 30 } }, { projection: { name: 1, age: 1 } })
+            .toArray()
+        res.status(200).send(result)
+    } catch (error) {
+        next(error)
+    }
+}
+export const deleteUser = async (req, res, next) => {
+    try {
+        const db = await connectDB()
+        const users = db.collection('users')
+        const result = await users.deleteOne({ _id: new ObjectId(req.params.id) })
+        if (result.deletedCount === 0) {
+            return res.status(404).send('User not found')
+        }
+        res.status(200).send(`User with id ${req.params.id} deleted`)
+    } catch (error) {
+        next(error)
+    }
+}
+
+export const deleteUsers = async (req, res, next) => {
+    try {
+        const db = await connectDB()
+        const users = db.collection('users')
+        const result = await users.deleteMany({ age: { $gte: 30 } })
+        res.status(200).send(`Deleted ${result.deletedCount} users`)
+    } catch (error) {
+        next(error)
+    }
+}
+export const updateUser = async (req, res, next) => {
+    try {
+        const db = await connectDB()
+        const users = db.collection('users')
+        const result = await users.updateOne({ _id: new ObjectId(req.params.id) }, { $set: req.body })
+        if (result.matchedCount === 0) {
+            return res.status(404).send('User not found')
+        }
+        res.status(200).send(`User with id ${req.params.id} updated`)
+    } catch (error) {
+        next(error)
+    }
+}
+
+export const updateUsers = async (req, res, next) => {
+    try {
+        const db = await connectDB()
+        const users = db.collection('users')
+        const result = await users.updateMany({ age: { $gte: 30 } }, { $inc: { age: 1 } })
+        res.status(200).send(`Updated ${result.modifiedCount} users`)
+    } catch (error) {
+        next(error)
+    }
+}
+
+
+
+
